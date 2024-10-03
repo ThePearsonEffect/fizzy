e3991b7b1d86de167306908d06dc522c50da577a
ThePearsonEffect <pearsonadrian79@gmail.com>
2024-10-03 09:37:31 -0500

    commit

---
+++ main.js
@@ -0,0 +1,26 @@
+/******************************
+ * YOUR CODE GOES RIGHT HERE! *
+ ******************************/
+//Fizz Buzz
+function FizzBuzz(value1, value2){
+  let returnValue = "";
+  for(let i =1; i<=100;i++){
+    if()
+  }
+}
+
+
+/**********************************
+ *      OUR CODE GOES BELOW.      *
+ * THIS IS FOR INTERNAL USE ONLY. *
+ * DO NOT ADD OR CHANGE ANYTHING! *
+ **********************************/
+
+
+if (typeof fizzy === "undefined") {
+  fizzy = undefined;
+}
+
+module.exports = {
+  fizzy,
+};
