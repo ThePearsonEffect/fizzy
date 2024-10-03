2174a489f5c17e0c77e65b9b0e991ba3f7a6916e
ThePearsonEffect <pearsonadrian79@gmail.com>
2024-10-03 09:49:16 -0500

    commit

--- main.js
+++ main.js
@@ -5,8 +5,20 @@
 function FizzBuzz(value1, value2){
   let returnValue = "";
   for(let i =1; i<=100;i++){
-    if()
+    if(i%value1==0 && i%value2==0){
+        returnValue += 'FizzBuzz ';
+    }
+    else if (i%value1==0){
+      returnValue += 'Fizz ';
+    }
+    else if (i%value2 == 0){
+        returnValue += 'Buzz ';
+    }
+    else{
+        returnVAlue += i + '';
+    }
   }
+  return returnValue;
 }
 
 
